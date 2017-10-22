273b9c13b359cbcf5160f0c0267cc53d71fbb9b3
Oliver Schulz <oschulz@mpp.mpg.de>
2017-10-22 10:03:51 +0200

    Use Compat.Test instead of Base.Test for Julia v0.7 compatibility

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 # This file is a part of PropDicts.jl, licensed under the MIT License (MIT).
 
-@Base.Test.testset "Package PropDicts" begin
+import Compat.Test
+Test.@testset "Package PropDicts" begin
     include.([
     ])
 end
