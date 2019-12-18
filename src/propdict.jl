66a870087f57e2324a2b0f15eee5451ddf95e1b1
Oliver Schulz <oschulz@mpp.mpg.de>
2019-12-18 16:36:39 +0100

    Make PropDict an AbstractDict and support getproperty

--- src/propdict.jl
+++ src/propdict.jl
@@ -1,20 +1,52 @@
 # This file is a part of PropDicts.jl, licensed under the MIT License (MIT).
 
+
+"""
+    PropDict <: AbstractDict{Union{Symbol,Int},Any}
+
+PropDicts is a dictionary for that supports
+
+Constructors:
+
+    PropDict(dict::AbstractDict)
+
+    PropDict(key1 => value, key2 => value2, ...)
+
+During construction, keys are automatically converted to `Symbol`s and
+`Int`s, values that are dicts are converted to `PropDict`s.
+
+`PropDict` support deep merging:
+```julia
+x = PropDict(:a => PropDict(:b => 7, :c => 5, :d => 2), :e => "foo")
+y = PropDict(:a => PropDict(:c => 42, :d => nothing), :f => "bar")
+
+z = merge(x, y)
+@assert z == PropDict(
+    :a => PropDict(:b=>7, :d => nothing, :c => 42),
+    :e => "foo", :f => "bar"
+)
+
+PropDicts.trim_null!(z)
+@assert z == PropDict(
+    :a => PropDict(:b=>7, :c => 42),
+    :e => "foo", :f => "bar"
+)
+```
+"""
+struct PropDict <: AbstractDict{Union{Symbol,Int},Any}
+    _internal_dict::Dict{Union{Symbol,Int},Any}
+
+    PropDict() = new(Dict{Union{Symbol,Int},Any}())
+
+    PropDict(dict::PropDict) = new(_dict(dict))
+
+    PropDict(dict::Dict{Union{Symbol,Int},Any}) = is_props_dict_compatible(dict) ? new(dict) : convert(PropDict, dict)
+end
 export PropDict
 
-struct PropDict
-    _internal_dict::Dict{Any,Any}
+PropDict(dict::AbstractDict) = convert(PropDict, dict)
 
-    PropDict() = new(Dict{Any,Any}())
-
-    function PropDict(dict::AbstractDict; auto_convert::Bool = true)
-        if auto_convert
-            new(is_props_dict(dict) ? dict : to_props_dict(dict))
-        else
-            new(dict)
-        end
-    end
-end
+PropDict(keys_and_values::Pair...) = PropDict(Dict(keys_and_values...))
 
 
 _dict(p::PropDict) = getfield(p, :_internal_dict)
@@ -22,35 +54,16 @@ _dict(p::PropDict) = getfield(p, :_internal_dict)
 Base.parent(p::PropDict) = _dict(p)
 
 
-import Base.==
-==(a::PropDict, b::PropDict) = _dict(a) == _dict(b)
+is_props_dict_compatible(d::AbstractDict) = false
 
-Base.convert(::Type{PropDict}, s::AbstractString) =  PropDict(JSON.parse(s))
-
-Base.print(io::IO, p::PropDict) = JSON.print(io, _dict(p))
-
-
-function Base.merge!(p::PropDict, others::PropDict...)
-    PropDict(deepmerge!(_dict(p), map(_dict, (others))...))
-    p
-end
-
-
-Base.merge(p::PropDict, others::PropDict...) =
-    PropDict(deepmerge(_dict(p), map(_dict, (others))...))
-
-
-const integer_expr = r"^[+-]?[0-9]+$"
-
-
-function is_props_dict(d::Dict{Any,Any})
+function is_props_dict_compatible(d::Dict{Union{Symbol,Int},Any})
     for (k, v) in d
         if !(isa(k, Symbol) || isa(k, Int))
             return false
         end
 
         if isa(v, AbstractDict)
-            if !is_props_dict(v)
+            if !is_props_dict_compatible(v)
                 return false
             end
         end
@@ -59,11 +72,11 @@ function is_props_dict(d::Dict{Any,Any})
     return true
 end
 
-is_props_dict(d::AbstractDict) = false
 
+Base.convert(::Type{PropDict}, d::PropDict) = d
 
-function to_props_dict(d::AbstractDict)
-    result = Dict{Any,Any}()
+function Base.convert(::Type{PropDict}, d::AbstractDict)
+    result = PropDict()
 
     for (k, v) in d
         k_new = if isa(k, Symbol) || isa(k, Int)
@@ -81,7 +94,7 @@ function to_props_dict(d::AbstractDict)
         end
 
         v_new = if isa(v, AbstractDict)
-            to_props_dict(convert(Dict{Any,Any}, v))
+            convert(PropDict, v)
         else
             v
         end
@@ -91,6 +104,63 @@ function to_props_dict(d::AbstractDict)
 
     result
 end
+
+
+import Base.==
+==(a::PropDict, b::PropDict) = _dict(a) == _dict(b)
+
+
+@inline Base.keys(p::PropDict) = keys(_dict(p))
+
+@inline Base.values(p::PropDict) = values(_dict(p))
+
+@inline function Base.getproperty(p::PropDict, s::Symbol)
+    if s == :_internal_dict
+        getfield(p, :_internal_dict)
+    else
+        _dict(p)[s]
+    end
+end
+
+@inline function Base.propertynames(p::PropDict, private::Bool = false)
+    names = collect(filter(x -> x isa Symbol, keys(_dict(p))))
+    if private
+        [names..., :_internal_dict]
+    else
+        names
+    end
+end
+
+
+Base.length(p::PropDict) = length(_dict(p))
+
+Base.getindex(p::PropDict, key) = getindex(_dict(p), key)
+
+Base.setindex!(p::PropDict, value, key) = setindex!(_dict(p), value, key)
+
+Base.delete!(p::PropDict, key) = delete!(_dict(p), key)
+
+Base.convert(::Type{PropDict}, s::AbstractString) =  PropDict(JSON.parse(s))
+
+Base.print(io::IO, p::PropDict) = JSON.print(io, _dict(p))
+
+Base.iterate(p::PropDict) = iterate(_dict(p))
+Base.iterate(p::PropDict, i) = iterate(_dict(p), i)
+
+
+function Base.merge!(p::PropDict, others::PropDict...)
+    PropDict(deepmerge!(_dict(p), map(_dict, (others))...))
+    p
+end
+
+
+Base.merge(p::PropDict, others::PropDict...) =
+    PropDict(deepmerge(_dict(p), map(_dict, (others))...))
+
+
+const integer_expr = r"^[+-]?[0-9]+$"
+
+
 
 
 function Base.read(::Type{PropDict}, filename::AbstractString; subst_pathvar::Bool = false, subst_env::Bool = false, trim_null::Bool = false)
--- test/test_propdict.jl
+++ test/test_propdict.jl
@@ -4,17 +4,29 @@ using PropDicts
 using Test
 
 @testset "propdict" begin
-    pa = PropDict(Dict("foo" => 11, "bar" => Dict("baz" => 42), "44" => "abc"))
-    pb = PropDict(Dict("foo" => 13, :bar => Dict(:baz => raw"$somevar")))
+    da = Dict("foo" => 11, "bar" => Dict("baz" => 42), "44" => "abc")
+    @test @inferred(PropDict(da)) isa PropDict
+
+    pa = PropDict(da)
+    @test parent(pa) === PropDicts._dict(pa)
+
+    @test sort(@inferred propertynames(pa)) == [:bar, :foo]
+    @test sort(propertynames(pa, true)) == [:_internal_dict, :bar, :foo]
+
+    pb = PropDict(Dict("foo" => 13, :bar => PropDict(:baz => raw"$somevar")))
 
     @test pa == deepcopy(pa)
     @test pa != pb
 
+    @test pa.foo == pa[:foo] == 11
+    @test pa.bar == pa[:bar] == PropDict(:baz => 42)
+    @test pa[44] == "abc"
+
     @test @inferred(merge(pa, pb)) isa PropDict
     pc = merge(pa, pb)
-    @test parent(pc)[:foo] == 13
-    @test parent(pc)[:bar] == Dict(:baz => raw"$somevar")
-    @test parent(pc)[44] == "abc"
+    @test pc.foo == 13
+    @test pc.bar == PropDict(:baz => raw"$somevar")
+    @test pc[44] == "abc"
 
     @test PropDicts.contains_vars(raw"fo\\$o") == true
     @test PropDicts.substitute_vars(raw"foo $bar ${baz} y", ignore_missing = true) == raw"foo $bar ${baz} y"
